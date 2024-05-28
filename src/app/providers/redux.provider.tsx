1dd6363169c1c7a440f8f864f6685919f9e45f8b
taeo <ohtk1119@gmail.com>
2024-05-28 16:24:57 +0900

    refactor provider

--- src/app/providers/redux.provider.tsx
+++ src/app/providers/redux.provider.tsx
@@ -2,8 +2,6 @@ import type { PropsWithChildren } from "react";
 import type { Store } from "redux";
 import { Provider } from "react-redux";
 
-const ReduxProvider = ({ store, children }: PropsWithChildren<{ store: Store<any, any, any> }>) => {
-  return <Provider store={store}>{children}</Provider>;
-};
+const ReduxProvider = ({ store, children }: PropsWithChildren<{ store: Store<any, any, any> }>) => <Provider store={store}>{children}</Provider>;
 
 export default ReduxProvider;
